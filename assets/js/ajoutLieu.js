ebc55b73f1411cd2e8bcafa80ce9c30f5705de92
emile45 <79720396+emile45@users.noreply.github.com>
2021-05-28 21:50:50 -0400

    ajustement
    J'ai enlevé une ligne de code qui servait a rien.

--- assets/js/ajoutLieu.js
+++ assets/js/ajoutLieu.js
@@ -2,7 +2,6 @@ function afficherAjoutLieu(){
 	bloc = document.getElementById('ajoutLieu')
 	if(bloc.style.display === 'inline-block'){
 		bloc.style.display = 'none';
-		bloc.style.paddingLeft = 10%;
 	}else{ //Afficher le bloc s'il ne l'est pas
 		bloc.style.display = 'inline-block';
 	}
